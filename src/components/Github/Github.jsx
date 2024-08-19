a4577d32e7a1fcd015e3edb17fbe0afb1dd7b2cb
Ayush H <jqghebri@gmail.com>
2024-08-19 19:24:36 +0530

    creation

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Github() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users/argus-66")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+
   return (
-    <div>Github</div>
-  )
+    <div className="text-center m-4 bg-gray-600 text-white text-3xl p-4">
+      Github followers: {data.followers}
+      <img src={data.avatar_url} alt="Git picture" width={300}/>
+      <h2 className="p-4">{data.login}</h2>
+    </div>
+  );
 }
 
-export default Github
+export default Github;
