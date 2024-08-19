ddff0d59a880b036cd18b2b6240507ad73eaa4a5
Ayush H <jqghebri@gmail.com>
2024-08-19 19:30:35 +0530

    creation

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/argus-66")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, []);
+    const data = useLoaderData()
+//   const [data, setData] = useState([]);
+//   useEffect(() => {
+//     fetch("https://api.github.com/users/argus-66")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data);
+//         setData(data);
+//       });
+//   }, []);
 
   return (
     <div className="text-center m-4 bg-gray-600 text-white text-3xl p-4">
       Github followers: {data.followers}
       <img src={data.avatar_url} alt="Git picture" width={300}/>
-      <h2 className="p-4">{data.login}</h2>
+      <h2 className="p-4">Username: {data.login}</h2>
     </div>
   );
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+    const response = await fetch("https://api.github.com/users/argus-66")
+    return response.json()
+}
